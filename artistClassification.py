dea9533caaf54137c4a48bbe818396e6adf4adca
Ruwanari Heenkenda <ruwanariheenkenda@gmail.com>
2019-05-17 16:04:43 +0530

    gmm classifier inserted

--- artistClassification.py
+++ artistClassification.py
@@ -4,21 +4,26 @@ Created on Mon May 13 16:07:54 2019
 
 @author: Ruwanari
 """
-
+import pickle
 import librosa
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
+from matplotlib.patches import Ellipse
 import os
 import csv
 # Preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.cluster import KMeans
+from sklearn.mixture import GaussianMixture
 #Keras
 import keras
 from keras import models
 from keras import layers
+
+
+        
 
 data = pd.read_csv('data.csv')
 data.head()
@@ -28,7 +33,7 @@ data.head()
 genre_list = data.iloc[:, -1]
 encoder = LabelEncoder()
 y = encoder.fit_transform(genre_list)
-print(y)
+
 # normalizing
 scaler = StandardScaler()
 X = scaler.fit_transform(np.array(data.iloc[:, :-1], dtype = float))
@@ -36,7 +41,16 @@ X = scaler.fit_transform(np.array(data.iloc[:, :-1], dtype = float))
 # spliting of dataset into train and test dataset
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
+amaradewa = X[:4]
+dest = "dest\\"
+print (amaradewa)
+gmm = GaussianMixture(n_components = 4, max_iter = 200, covariance_type='diag',n_init = 3)
+gmm.fit(amaradewa)
+picklefile = "amaradewa.gmm"
+pickle.dump(gmm,open(dest + picklefile,'wb'))
+print (' modeling completed for speaker: amaradewa')
 
+'''
 # creating a model
 model = models.Sequential()
 model.add(layers.Dense(256, activation='relu', input_shape=(X_train.shape[1],)))
@@ -67,3 +81,4 @@ print('test_acc: ',test_acc)
 # predictions
 predictions = model.predict(X_test)
 np.argmax(predictions[0])
+'''
